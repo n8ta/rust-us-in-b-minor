c161c29d7304515b06aaacd2751c57f1442a4c78
n8ta <n8@u.northwestern.edu>
2021-05-10 17:08:45 -0500

    pointers!

--- src/lib/librustbare/ffi.rb
+++ src/lib/librustbare/ffi.rb
@@ -5,5 +5,6 @@ module Fib
     extend Fiddle::Importer
     dlload File.expand_path('./librustbare.dylib', __dir__)
     extern 'void hello_world(uint32_t x)'
+    extern 'int32_t sum_array(size_t size, const int32_t* array)'
   end
 end
