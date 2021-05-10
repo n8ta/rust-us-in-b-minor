55de86b968980f9d7fb49947940d6caef0c11996
n8ta <n8@u.northwestern.edu>
2021-05-10 15:56:34 -0500

    outline barerust

---
+++ src/lib/bare-rust.rb
@@ -0,0 +1,24 @@
+require_relative 'librustbare/ffi'
+
+# Hello from RUST!
+puts Fib::FFI.helgitlo_world(1337)
+
+
+class BareRust
+  def self.encode(msg, schema, type=nil)
+    # TODO:
+  end
+
+  def self.decode(msg, schema, type=nil)
+    # TODO:
+  end
+
+  def self.Int
+    # TODO:
+  end
+
+  def self.Array
+    # TODO:
+  end
+
+end
--- src/lib/barerbrs.rb
+++
@@ -1,4 +0,0 @@
-require_relative 'librustbare/ffi'
-
-# Hello from RUST!
-puts Fib::FFI.hello_world(1337)
