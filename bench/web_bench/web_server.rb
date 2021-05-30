938cf87530e8df40b4bbe613318d546c7f461059
n8ta <n8@u.northwestern.edu>
2021-05-30 15:04:07 -0500

    spacing

--- bench/web_bench/web_server.rb
+++ bench/web_bench/web_server.rb
@@ -13,7 +13,6 @@ else
   exit(-1)
 end
 
-
 port_to_listen_to = 8081
 
 request_schema = Bare.Array(Bare.I16)
@@ -26,16 +25,10 @@ end
 
 server = TCPServer.open(port_to_listen_to)
 
-# def bin_to_hex(binary_string)
-#   binary_string.unpack('H*').first
-# end
-
 loop {
   client = server.accept
   Thread.new do
-    # db = database.dup
     loop do
-
       eight_bytes = client.recv(8)
       request_size = Bare.decode(eight_bytes, Bare.I64)
       request = client.recv(request_size)
